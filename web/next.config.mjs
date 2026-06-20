6bf1ed5c1d0afb7c338416085c089ea00a0ac381
Omar Maher <omarmaher23942@gmail.com>
2026-06-20 05:02:51 +0300

    fix: remove double /api in proxy

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       return [
         {
           source: "/backend/:path*",
-          destination: "http://153.92.221.161:8001/api/:path*",
+          destination: "http://153.92.221.161:8001/:path*",
         },
       ];
     },
